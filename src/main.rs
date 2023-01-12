338c2c5bce08187949ee0c224e5d879ee833f7f8
Tristan F <LeoDog896@hotmail.com>
2023-01-12 17:43:46 +0000

    feat: implement basic methods
    (memory-only with actix_storage)

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
-use actix_web::{get, post, delete, web, App, HttpResponse, HttpServer, Responder};
-
-struct AppState {
-    database: HashMap<String, String>
-}
+use actix_storage::Storage;
+use actix_storage_hashmap::HashMapStore;
+use actix_web::{error, Error, delete, get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/{key}")]
-async fn get(path: web::Path<(String,)>) -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn get(storage: Storage, path: web::Path<(String,)>) -> Result<impl Responder, Error> {
+    let value = storage.get_bytes(&path.0).await?;
+    if let Some(value) = value {
+        Ok(HttpResponse::Ok().body(value))
+    } else {
+        Err(error::ErrorNotFound("Not found."))
+    }
 }
 
 #[delete("/{key}")]
-async fn del(path: web::Path<(String,)>) -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn del(storage: Storage, path: web::Path<(String,)>) -> Result<impl Responder, Error> {
+    storage.delete(&path.0).await?;
+    Ok(HttpResponse::Ok())
 }
 
 #[post("/")]
-async fn add(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+async fn add(storage: Storage, req_body: String) -> Result<impl Responder, Error> {
+    let mut body_set = req_body.split('=');
+    let key = body_set.next().ok_or_else(|| error::ErrorBadRequest("Key Required."))?;
+    let value = body_set.next().ok_or_else(|| error::ErrorBadRequest("Key Required."))?;
+    storage.set_bytes(key, value).await?;
+    Ok(HttpResponse::Ok())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let store = HashMapStore::new();
+
+    let storage = Storage::build().store(store).finish();
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(storage.clone())
             .service(get)
             .service(del)
             .service(add)
